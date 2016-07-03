4a2e7427c78d6d039ea733b35f7da810cccd2133
Karol Wojtyła <kamil9772@wp.pl>
2016-07-03 21:43:53 +0200

    Working on serialization with BinaryFormatter; Need to resolve problem how to save/load string data.

--- Wiedzowkonator/MainWindow.xaml.cs
+++ Wiedzowkonator/MainWindow.xaml.cs
@@ -16,10 +16,14 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Drawing;
 //
 using Application = System.Windows.Application;
 using MessageBox = System.Windows.Forms.MessageBox;
 using KeyEventArgs = System.Windows.Input.KeyEventArgs;
+using Image = System.Windows.Controls.Image;
 
 namespace Wiedzowkonator
 {
@@ -30,8 +34,11 @@ namespace Wiedzowkonator
     [Serializable]
     public class SerializationData
     {
-        public Image[] screenshots_;
-        public BitmapImage[] bitmapImage_;
+        //public Image[] screenshots_;
+        //public BitmapImage[] bitmapImage_;
+        public List<string> answeredScreenshots = new List<string>();
+        public string[] points = new string[1];
+        public string[] participants = new string[1];
     }
 
     public partial class MainWindow : Window
@@ -40,9 +47,12 @@ namespace Wiedzowkonator
         public Image[] screenshots;
         public bool screenshotQuizStarted;
         public string path = "E:/screeny/";
+        public string[] nameOfScreenshots;
         BitmapImage[] bitmapImage;
         int lastScreenshotIndex;
         int screenshotsCompleted;
+
+
         enum quizState { choosingQuestion, answeringQuestion, givingPoints };
         quizState curQuizState;
 
@@ -51,6 +61,7 @@ namespace Wiedzowkonator
         public MainWindow()
         {
             InitializeComponent();
+            Application.Current.MainWindow.WindowState = WindowState.Maximized;
             //Start();
         }
 
@@ -63,7 +74,7 @@ namespace Wiedzowkonator
 
             plusFirstParticipant.Width = 0;
             minutFirstParticipant.Width = 0;
-            nameFisrtParticipant.Width = 0;
+            nameFirstParticipant.Width = 0;
             pointsFirstParticipant.Width = 0;
             confirmPoints.Width = 0;
 
@@ -136,7 +147,7 @@ namespace Wiedzowkonator
             {
                 plusFirstParticipant.Width = 33;
                 minutFirstParticipant.Width = 33;
-                nameFisrtParticipant.Width = 120;
+                nameFirstParticipant.Width = 120;
                 pointsFirstParticipant.Width = 33;
                 confirmPoints.Width = 75;
             }
@@ -144,7 +155,7 @@ namespace Wiedzowkonator
             {
                 plusFirstParticipant.Width = 0;
                 minutFirstParticipant.Width = 0;
-                nameFisrtParticipant.Width = 0;
+                nameFirstParticipant.Width = 0;
                 pointsFirstParticipant.Width = 0;
                 confirmPoints.Width = 0;
             }
@@ -187,6 +198,8 @@ namespace Wiedzowkonator
             curQuizState = quizState.choosingQuestion;
             ShowingHUD();
             screenshots[lastScreenshotIndex] = null;
+            serializationData.answeredScreenshots.Add(nameOfScreenshots[lastScreenshotIndex]); //Adding name of completed question
+            MessageBox.Show(serializationData.answeredScreenshots[0]);
             screenshotsCompleted++;
             for (int i = lastScreenshotIndex; i < screenshots.Length - 1; i++)
             {
@@ -224,13 +237,14 @@ namespace Wiedzowkonator
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
             fileDialog.Multiselect = true;
-
+            
             if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 string[] path = fileDialog.FileNames;
                 //Initialize new array sizes depending of screenshot numbers
                 bitmapImage = new BitmapImage[path.Length];
                 screenshots = new Image[bitmapImage.Length];
+                nameOfScreenshots = new string[bitmapImage.Length];
                 int index = 0; //Index of each screenshot
                 foreach (string screen in path)
                 {
@@ -239,6 +253,7 @@ namespace Wiedzowkonator
                     screenshots[index].Source = bitmapImage[index];
                     screenshots[index].Width = bitmapImage[index].Width;
                     screenshots[index].Height = bitmapImage[index].Height;
+                    nameOfScreenshots[index] = screen;
 
                     index++;
                 }
@@ -246,7 +261,7 @@ namespace Wiedzowkonator
         }
 
         private void Save(object sender, RoutedEventArgs e)
-        {
+        {           
             SaveFileDialog fileDialog = new SaveFileDialog();
             if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
@@ -257,15 +272,40 @@ namespace Wiedzowkonator
 
                 SerializationData data = new SerializationData();
 
-                data.bitmapImage_ = bitmapImage;
-                data.screenshots_ = screenshots;
-                bf.Serialize(stream, serializationData);
+                data.answeredScreenshots = serializationData.answeredScreenshots;
+                data.points[0] = pointsFirstParticipant.Text;
+                data.participants[0] = nameFirstParticipant.Text;
+
+                bf.Serialize(stream, data);
                 stream.Close();
             }
+            /*
+            SaveFileDialog fileDialog = new SaveFileDialog();
+            if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string path = fileDialog.FileName;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    BitmapImage __bitmapImage = new BitmapImage();
+                    __bitmapImage = bitmapImage[0];
+                    
+                    PngBitmapEncoder encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(__bitmapImage));
+                    encoder.Save(ms);
+
+                    using (FileStream file = new FileStream(path + ".bin", FileMode.Create, System.IO.FileAccess.Write))
+                    {
+                        byte[] bytes = new byte[ms.Length];
+                        ms.Read(bytes, 0, (int)ms.Length);
+                        file.Write(bytes, 0, bytes.Length);
+                        ms.Close();
+                    }
+                }
+            }*/
         }
 
         private void Load(object sender, RoutedEventArgs e)
-        {
+        {           
             OpenFileDialog fileDialog = new OpenFileDialog();
             if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
@@ -275,14 +315,78 @@ namespace Wiedzowkonator
                 FileStream stream = File.Open(path, FileMode.Open);
 
                 SerializationData data = (SerializationData)bf.Deserialize(stream);
+                serializationData.answeredScreenshots = data.answeredScreenshots;
+                nameFirstParticipant.Text = data.participants[0];
+                pointsFirstParticipant.Text = data.points[0];
+
                 //Initializing arrays with imported data length and assigning new values
+                /*
                 bitmapImage = new BitmapImage[data.bitmapImage_.Length];
                 screenshots = new Image[data.screenshots_.Length];
                 screenshots = data.screenshots_;
-                bitmapImage = data.bitmapImage_;
+                bitmapImage = data.bitmapImage_;*/
 
                 stream.Close();
-            }
+                MessageBox.Show(serializationData.answeredScreenshots.Count.ToString());
+                MessageBox.Show(nameOfScreenshots.Length.ToString());
+
+                for (int i = 0; i < serializationData.answeredScreenshots.Count; i++)
+                {
+                    for (int j = 0; j < nameOfScreenshots.Length; j++)
+                    {
+                        if (serializationData.answeredScreenshots[i] == nameOfScreenshots[j])
+                        {
+                            MessageBox.Show("i = " + i + "; j = " + j);
+                            screenshotsCompleted++;
+                            screenshots[i] = null;
+
+                            for (int k = lastScreenshotIndex; k < screenshots.Length - 1; k++)
+                            {
+                                screenshots[k] = screenshots[k + 1];
+                            }
+                        }
+                    }
+                }
+            }            
+                /*
+                string path = fileDialog.FileName;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        byte[] bytes = new byte[file.Length];
+                        file.Read(bytes, 0, (int)file.Length);
+                        ms.Write(bytes, 0, (int)file.Length);
+
+                        //PngBitmapDecoder decoder = new PngBitmapDecoder(ms, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+                        /*
+                        BitmapImage[] __bitmapImage = new BitmapImage[6];
+                        BitmapFrame[] bitmapFrame = new BitmapFrame[6];
+
+                        Stream imageStreamSource = new FileStream("E:/screeny/1.png", FileMode.Open, FileAccess.Read, FileShare.Read);
+                        PngBitmapDecoder decoder = new PngBitmapDecoder(file, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+                        BitmapSource bitmapSource = decoder.Frames[0];
+
+                        Image image = new Image();
+                        image.Source = bitmapSource;
+                        image.Width = bitmapSource.Width;
+                        image.Height = bitmapSource.Height;
+
+                        canvasScreenshotQuiz.Children.Add(image);   */
+                        /*
+                        BitmapImage bitmap = new BitmapImage();
+
+
+                        Image image = new Image();
+                        image.Source = bitmap;
+                        image.Width = bitmap.Width;
+                        image.Height = bitmap.Height;
+
+                        canvasScreenshotQuiz.Children.Add(image);
+                        if (canvasScreenshotQuiz.Children.Count > 0)
+                            MessageBox.Show(image.Width.ToString() + "x" + image.Height.ToString() + "\n" + image.Source.ToString());
+                    }
+                }*/      
         }
     }
 }
